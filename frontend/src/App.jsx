e6ce581361ffab97e0afc81a7b41e45042f34d64
tyagisarthak13 <sarthaktyagi797@gmail.com>
2025-06-18 21:34:33 +0530

    added render url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,9 +21,12 @@ function App() {
   }, []);
 
   async function reviewCode() {
-    const response = await axios.post("http://localhost:3000/ai/get-review", {
-      code,
-    });
+    const response = await axios.post(
+      "https://ai-code-review-r18m.onrender.com",
+      {
+        code,
+      }
+    );
 
     setReview(response.data);
   }
